5e683f6d54124a2c5ad6adff7fa86ac5fac02ad1
ubuntu <kouskund@in.ibm.com>
2019-03-18 11:32:49 +0000

    Service test

--- src/test/java/com/stackroute/muzixservice/service/MuzixServiceTest.java
+++ src/test/java/com/stackroute/muzixservice/service/MuzixServiceTest.java
@@ -4,8 +4,10 @@ import com.stackroute.muzixservice.domain.Artist;
 import com.stackroute.muzixservice.domain.Image;
 import com.stackroute.muzixservice.domain.Track;
 import com.stackroute.muzixservice.exception.TrackAlreadyExistsException;
+import com.stackroute.muzixservice.exception.TrackNotFoundException;
 import com.stackroute.muzixservice.repository.MuzixRepository;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -15,6 +17,10 @@ import org.mockito.MockitoAnnotations;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 public class MuzixServiceTest {
 
@@ -33,10 +39,10 @@ public class MuzixServiceTest {
 
     @Before
     public void setUp(){
-        MockitoAnnotations.initMocks(testClass:  this);
-        image = new Image( imageId: 1, imageUrl: "http:url" , imageSize="large");
-        artist = new Artist( artisId: 101, artistName: "Jonhn", url: "new url" ,image);
-        track = new Track( trackId: "Track1", trackName: "MyNewTracks",comments:"new comments", trackListeners: "123" , trackUrl: "new track url",artist);
+        MockitoAnnotations.initMocks(this);
+        image = new Image( 1, "http:url" , "large");
+        artist = new Artist( 101, "Jonhn", "new url" ,image);
+        track = new Track( "Track1", "MyNewTracks","new track url", "123" , "new comment",artist);
         listTrack = new ArrayList<>();
         listTrack.add(track);
         optional=Optional.of(track);
@@ -53,6 +59,59 @@ public class MuzixServiceTest {
     public void teatSaveTrackSuccess () throws TrackAlreadyExistsException {
 
         when(muzixRepository.insert(track)).thenReturn(track);
+        Track fetchTrack = muzixService.SaveTrackToWishList(track);
+        Assert.assertEquals(track,fetchTrack);
+        verify(muzixRepository,times( 1)).insert(track);
+        verify(muzixRepository,times(1)).findById(track.getTrackId());
 
     }
+
+    @Test(expected = TrackAlreadyExistsException.class)
+    public void teatSaveTrackFailure () throws TrackAlreadyExistsException {
+
+        when(muzixRepository.insert(track)).thenReturn(track);
+        when(muzixRepository.findById(track.getTrackId())).thenReturn(optional);
+        Track fetchTrack = muzixService.SaveTrackToWishList(track);
+        Assert.assertEquals(track,fetchTrack);
+        verify(muzixRepository,times( 1)).insert(track);
+        verify(muzixRepository,times(1)).findById(track.getTrackId());
+
+    }
+
+    @Test
+    public void teatUpdateCommentSuccess () throws TrackNotFoundException {
+
+        when(muzixRepository.findById(track.getTrackId())).thenReturn(optional);
+        track.setComments("comments  updated");
+        Track fetchTrack = muzixService.updateCommentForTrack(track.getComments(),track.getTrackId());
+        Assert.assertEquals(fetchTrack.getComments(),"comments  updated");
+
+        verify(muzixRepository,times( 1)).save(track);
+        verify(muzixRepository,times(2)).findById(track.getTrackId());
+
+    }
+
+    @Test
+    public void teatDeleteTrack () throws TrackNotFoundException {
+
+        when(muzixRepository.findById(track.getTrackId())).thenReturn(optional);
+        boolean fetchtrack = muzixService.deleteTrackFromWishList(track.getTrackId());
+        Assert.assertEquals(true,fetchtrack);
+
+        verify(muzixRepository,times(1)).findById(track.getTrackId());
+        verify(muzixRepository,times(1)).deleteById(track.getTrackId());
+
+    }
+
+    @Test
+    public void testGetAllTracks() throws Exception {
+
+        when(muzixRepository.findAll()).thenReturn(listTrack);
+        List<Track> list = muzixService.getAllTrackFromWishList();
+        Assert.assertEquals(list,listTrack);
+
+        verify(muzixRepository,times(1)).findAll();
+    }
+
+
 }
